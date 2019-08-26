f4aac1de98aa6c021bf992e4b606d8c599434dc3
mystborn <thefirewithin1017@gmail.com>
2019-08-25 19:21:05 -0500

    More math functions

--- include/su_math.h
+++ include/su_math.h
@@ -1,12 +1,26 @@
 #ifndef SDL_UTILS_MATH_H
 #define SDL_UTILS_MATH_H
 
+#include <math.h>
+
 static inline int fast_floor(float x) {
     return (int)(x + 32768.0f) - 32768;
 }
 
 static inline int fast_ceil(float x) {
     return 32768 - (int)(32768.f - x);
+}
+
+static inline float floor_ext(float value, float n) {
+    return floorf(value / n) * n;
+}
+
+static inline float ceil_ext(float value, float n) {
+    return ceilf(value / n) * n;
+}
+
+static inline float round_ext(float value, float n) {
+    return roundf(value / n) * n;
 }
 
 #endif
